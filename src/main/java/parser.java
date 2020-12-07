523b578b38c560488141fb3f7bac2fa5d5916f87
timoc <timo.castelberg@stud.unibas.ch>
2020-12-07 17:30:19 +0100

    added some debug information

--- src/main/java/parser.java
+++ src/main/java/parser.java
@@ -14,12 +14,12 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
-import java.awt.image.WritableRaster;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
@@ -171,6 +171,12 @@ public class parser {
         String path = deleteEnding(commentOrSubmission) + ".tsv";
         List<photoshop_out> outList = new ArrayList<>();
         List<String> unsupportedUrl = new ArrayList<>();
+        int counter_toplvl = 0;
+        int counter_score = 0;
+        int counter_imgSize = 0;
+        int counter_multiplImages = 0;
+        int counter_comments = 0;
+        int counter_success = 0;
 
         // Read json file to list
         ObjectMapper mapper = new ObjectMapper();
@@ -178,102 +184,136 @@ public class parser {
 
         for (comment comment : commentList) {
 
+            counter_comments++;
             // first check if it is a top level comment by looking at the prefix of parent_id="t3"_id
             // also look at the score to get rid of spam => score >= 20
             String parent_id = comment.getParent_id();
             String prefix = parent_id.substring(0, 2);
             int sc = Integer.parseInt(comment.getScore());
 
-            if (prefix.equals("t3") && (sc > 20)) {
-                //top lvl comment, now find link of image in the body
-                String bo = comment.getBody();
-                ArrayList<String> links = getUrlsFromBody(bo);
+            if (!prefix.equals("t3")) {
+                counter_toplvl++;
+                continue;
+            }
+            if (sc < 20) {
+                counter_score++;
+                continue;
+            }
 
-                if (links.size() == 1) { // we only consider comments with only one link
+            //top lvl comment, now find link of image in the body
+            String bo = comment.getBody();
+            ArrayList<String> links = getUrlsFromBody(bo);
 
-                    BufferedImage bimg = null;
-                    int imgCounterLink = 0;
+            if (!(links.size() == 1)) { // we only consider comments with only one link
+                counter_multiplImages++;
+                continue;
+            }
+            BufferedImage bimg = null;
+            int imgCounterLink = 0;
 
-                    boolean processedLink = false; //check if a link provides at least one image if not write to unsupported file
+            boolean processedLink = false; //check if a link provides at least one image if not write to unsupported file
 
-                    String link = links.get(0);
-                    String linkBuilder = "reddit.com/r/photoshopbattles/comments/" + comment.getParent_id().substring(3) + "/a/" + comment.getId();
+            String link = links.get(0);
+            String linkBuilder = "reddit.com/r/photoshopbattles/comments/" + comment.getParent_id().substring(3) + "/a/" + comment.getId();
+
+            try {
+                driver.get(link);
+                Thread.sleep(1500); // easiest way to wait till any page is loaded
+            } catch (Exception e) {
+                e.printStackTrace();
+                continue;
+            }
+
+
+            Document doc = Jsoup.parse(driver.getPageSource());
+
+            Elements img = doc.getElementsByTag("img");
+            String imgDuplicate = "";
+            //System.out.println(img.size()+ "  number of img elements found");
+            for (Element element : img) {
+                String src = element.absUrl("src");
+                if (src.toLowerCase().contains("i.imgur") || src.toLowerCase().contains("pinimg.com") || src.toLowerCase().contains("pbs.twigmg.com") || src.toLowerCase().contains("upload.wikimedia.org") || src.toLowerCase().contains("ytimg.com") || src.toLowerCase().contains("i.reddituploads.com") || src.toLowerCase().contains("puu.sh") || src.toLowerCase().contains("flickr.com") || src.toLowerCase().contains("deviantart.com") || src.toLowerCase().contains("en.wikipedia.org") || src.toLowerCase().contains("i.redd.it")) { //
+
+                    //      if (getFormat(src).contains("png") || getFormat(src).contains("jpg") || getFormat(src).contains("jpeg")) {
+
+                    //check for img duplicates, since images on e.g. imgur are present multiple times with different endings
+                    if (imgDuplicate.equals(deleteEnding(src))) {
+                        continue;
+                    }
+                    imgDuplicate = deleteEnding(src);
+
+                    URL url = null;
+                    try {
+                        url = new URL(src);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    if (url == null) {
+                        continue;
+                    }
 
                     try {
-                        driver.get(link);
-                        Thread.sleep(1500); // easiest way to wait till any page is loaded
+                        bimg = ImageIO.read(url);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    if (bimg == null) {
+                        continue;
+                    }
+
+                    byte[] imgByte = null;
+                    try {
+                        imgByte = getConnection(src).execute().bodyAsBytes();
                     } catch (Exception e) {
                         e.printStackTrace();
                         continue;
                     }
-
-
-                    Document doc = Jsoup.parse(driver.getPageSource());
-
-                    Elements img = doc.getElementsByTag("img");
-                    String imgDuplicate = "";
-                    //System.out.println(img.size()+ "  number of img elements found");
-                    for (Element element : img) {
-                        String src = element.absUrl("src");
-                        if (src.toLowerCase().contains("i.imgur") || src.toLowerCase().contains("pinimg.com") || src.toLowerCase().contains("pbs.twigmg.com") || src.toLowerCase().contains("upload.wikimedia.org") || src.toLowerCase().contains("ytimg.com") || src.toLowerCase().contains("i.reddituploads.com") || src.toLowerCase().contains("puu.sh") || src.toLowerCase().contains("flickr.com") || src.toLowerCase().contains("deviantart.com") || src.toLowerCase().contains("en.wikipedia.org") || src.toLowerCase().contains("i.redd.it")) { //
-
-                            //      if (getFormat(src).contains("png") || getFormat(src).contains("jpg") || getFormat(src).contains("jpeg")) {
-
-                            //check for img duplicates, since images on e.g. imgur are present multiple times with different endings
-                            if (imgDuplicate.equals(deleteEnding(src))) {
-                                continue;
-                            }
-                            imgDuplicate = deleteEnding(src);
-
-                            URL url = null;
-                            try {
-                                url = new URL(src);
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                            if (url == null) {
-                                continue;
-                            }
-                            //!!!!!!!!!!!!!!   try !!!!!!!!!!!!!
-                            try {
-                                bimg = ImageIO.read(url);
-                            }catch (Exception e){
-                                e.printStackTrace();
-                            }
-                           
-                            if (bimg == null) {
-                                continue;
-                            }
-
-                            WritableRaster raster = bimg.getRaster();
-                            DataBufferByte data = (DataBufferByte) raster.getDataBuffer();
-                            byte[] imgByte = data.getData();
-                            String idSuffix = String.valueOf(imgCounterLink);
-
-                            if (imgByte.length < 10000 || bimg.getWidth() < 300 || bimg.getHeight() < 300) { //check for min size of 10kB for and image and is at least 300x300
-                                continue;
-                            }
-
-                            //trim img url remove everything after question mark
-                            src = trimUrl(src);
-
-                            //create photoshop out which is the content of the output file
-                            photoshop_out po = new photoshop_out(comment.getId() + "_" + idSuffix, parent_id.substring(3), src, getFormat(src), sha256Hex(imgByte), String.valueOf(imgByte.length), comment.getScore(), comment.getAuthor(), linkBuilder, comment.getCreated_utc(), bimg.getWidth(), bimg.getHeight());
-                            outList.add(po);
-                            processedLink = true;
-                            //}
+                    String format = "";
+                    try {
+                        format = URLConnection.guessContentTypeFromStream(new ByteArrayInputStream(imgByte));
+                        if (format.startsWith("image/")) {
+                            format = format.substring(6);
                         }
-
+                    } catch (Exception e) {
+                        format = getFormat(src);
+                        e.printStackTrace();
                     }
-                    if (!processedLink) { //saves links which do not provide an image to an additional file
-                        unsupportedUrl.add(link);
 
+
+                    String idSuffix = String.valueOf(imgCounterLink);
+
+                    if (imgByte.length < 10000 || bimg.getWidth() < 300 || bimg.getHeight() < 300) { //check for min size of 10kB for and image and is at least 300x300
+                        counter_imgSize++;
+                        continue;
                     }
+
+                    //trim img url remove everything after question mark
+                    src = trimUrl(src);
+
+                    //create photoshop out which is the content of the output file
+                    photoshop_out po = new photoshop_out(comment.getId() + "_" + idSuffix, parent_id.substring(3), src, format, sha256Hex(imgByte), String.valueOf(imgByte.length), comment.getScore(), comment.getAuthor(), linkBuilder, comment.getCreated_utc(), bimg.getWidth(), bimg.getHeight());
+                    outList.add(po);
+                    processedLink = true;
+                    counter_success++;
+                    //}
                 }
+
             }
+            if (!processedLink) { //saves links which do not provide an image to an additional file
+                unsupportedUrl.add(link);
+
+            }
+
+
         }
+        unsupportedUrl.add(0, "Number of comments successful parsed:  " + counter_success);
+        unsupportedUrl.add(1, "Number of comments parsed:  " + counter_comments);
+        unsupportedUrl.add(2, "Number of comments which are not top lvl:   " + counter_toplvl);
+        unsupportedUrl.add(3, "Number of comments with score below 20  :  " + counter_score);
+        unsupportedUrl.add(4, "Number of comments with more than one image:  " + counter_multiplImages);
+        unsupportedUrl.add(5, "Number of comments with too small images:  " + counter_imgSize);
         finalizeOutput(outList, path, unsupportedUrl);
-        //driverC.quit();
     }
 
     private static void parseSubmissions(File f, String commentOrSubmission, WebDriver driver) throws Exception {
@@ -283,6 +323,10 @@ public class parser {
         List<original_out> outList = new ArrayList<>();
         List<String> unsupportedUrl = new ArrayList<>();
 
+        int counter_score = 0;
+        int counter_imgSize = 0;
+        int counter_submisssions = 0;
+        int counter_success = 0;
         // Read json file to list
         ObjectMapper mapper = new ObjectMapper();
         submission[] submissionList = mapper.readValue(f, submission[].class);
@@ -290,6 +334,7 @@ public class parser {
 
         BufferedImage bimg = null;
         for (submission sub : submissionList) {
+            counter_submisssions++;
             boolean processedSubmission = false;
 
             //create permalink referring to the post
@@ -299,6 +344,7 @@ public class parser {
             int sc = Integer.parseInt(sub.getScore());
 
             if (sc < 20) {  //score of post needs to be at least 20, to filter spam
+                counter_score++;
                 continue;
             }
             String ending = getFormat(sUrl);
@@ -339,23 +385,37 @@ public class parser {
                     continue;
                 }
 
-                WritableRaster raster = bimg.getRaster();
-                DataBufferByte data = (DataBufferByte) raster.getDataBuffer();
-                byte[] imgByte = data.getData();
+                byte[] imgByte = null;
+                try {
+                    imgByte = getConnection(sUrl).execute().bodyAsBytes();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    continue;
+                }
+                String format = "";
+                try {
+                    format = URLConnection.guessContentTypeFromStream(new ByteArrayInputStream(imgByte));
+                    if (format.startsWith("image/")) {
+                        format = format.substring(6);
+                    }
 
+                } catch (Exception e) {
+                    format = getFormat(sUrl);
+                    e.printStackTrace();
+                }
                 if (imgByte.length < 10000 || bimg.getHeight() < 300 || bimg.getWidth() < 300) {  //check for min size of 10kB for and image
+                    counter_imgSize++;
                     continue;
                 }
 
-                original_out po = new original_out(sub.getId(), sub.getUrl(), getFormat(sUrl), sha256Hex(imgByte), String.valueOf(imgByte.length), sub.getScore(), sub.getAuthor(), linkBuilder, sub.getCreated_utc(), bimg.getWidth(), bimg.getHeight());
+                original_out po = new original_out(sub.getId(), sub.getUrl(), format, sha256Hex(imgByte), String.valueOf(imgByte.length), sub.getScore(), sub.getAuthor(), linkBuilder, sub.getCreated_utc(), bimg.getWidth(), bimg.getHeight());
                 outList.add(po);
                 processedSubmission = true;
+                counter_success++;
 
 
             } //url does not directly refer to an image checkout provided url for images with headless Browser
             else if (!sUrl.equals("")) {
-
-                //WebDriver driverSubmissions =new FirefoxDriver(options);
 
                 //try {
                 try {
@@ -365,7 +425,6 @@ public class parser {
                     continue;
                 }
                 Thread.sleep(1500); // easiest way to wait till any page is loaded
-
 
                 Document doc;
                 try {
@@ -380,11 +439,9 @@ public class parser {
                 }
                 Elements img = doc.getElementsByTag("img");
 
-
                 String imgDuplicate = "";
                 for (Element element : img) {
                     String src = element.absUrl("src");
-                    // if (src.toLowerCase().contains("i.imgur")) { //
                     if (src.toLowerCase().contains("i.imgur") || src.toLowerCase().contains("pinimg.com") || src.toLowerCase().contains("pbs.twigmg.com") || src.toLowerCase().contains("upload.wikimedia.org") || src.toLowerCase().contains("ytimg.com") || src.toLowerCase().contains("i.reddituploads.com") || src.toLowerCase().contains("puu.sh") || src.toLowerCase().contains("flickr.com") || src.toLowerCase().contains("deviantart.com") || src.toLowerCase().contains("en.wikipedia.org") || src.toLowerCase().contains("i.redd.it")) { //
 
                         //check for img duplicates, since images on e.g. imgur are present multiple times with different endings
@@ -408,28 +465,38 @@ public class parser {
                         if (bimg == null) {
                             continue;
                         }
-
-                        WritableRaster raster = bimg.getRaster();
-                        DataBufferByte data = (DataBufferByte) raster.getDataBuffer();
-                        byte[] imgByte = data.getData();
+                        byte[] imgByte = null;
+                        try {
+                            imgByte = getConnection(sUrl).execute().bodyAsBytes();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            continue;
+                        }
+                        String format = "";
+                        try {
+                            format = URLConnection.guessContentTypeFromStream(new ByteArrayInputStream(imgByte));
+                            if (format.startsWith("image/")) {
+                                format = format.substring(6);
+                            }
+                        } catch (Exception e) {
+                            format = getFormat(src);
+                            e.printStackTrace();
+                        }
 
                         if (imgByte.length < 10000 || bimg.getHeight() < 300 || bimg.getWidth() < 300) { //check for min size of 10kB for and image
+                            counter_imgSize++;
                             continue;
                         }
                         //trim img url remove everything after question mark
                         src = trimUrl(src);
 
                         //create original_out which holds the content of the output file
-                        original_out po = new original_out(sub.getId(), src, getFormat(src), sha256Hex(imgByte), String.valueOf(imgByte.length), sub.getScore(), sub.getAuthor(), linkBuilder, sub.getCreated_utc(), bimg.getWidth(), bimg.getHeight());
+                        original_out po = new original_out(sub.getId(), src, format, sha256Hex(imgByte), String.valueOf(imgByte.length), sub.getScore(), sub.getAuthor(), linkBuilder, sub.getCreated_utc(), bimg.getWidth(), bimg.getHeight());
                         outList.add(po);
                         processedSubmission = true;
+                        counter_success++;
                     }
                 }
-
-
-                    /*}finally {
-                        driverSubmissions.quit();
-                    }*/
 
             } else {//url is empty
                 emptyURL++;
@@ -439,21 +506,13 @@ public class parser {
             }
         }
         unsupportedUrl.add("Number of empty url:  " + emptyURL);
+        unsupportedUrl.add(0, "Number of submissions successful parsed:  " + counter_success);
+        unsupportedUrl.add(1, "Number of submissions parsed:  " + counter_submisssions);
+        unsupportedUrl.add(2, "Number of submissions with score below 20:  " + counter_score);
+        unsupportedUrl.add(3, "Number of submissions with too small images:  " + counter_imgSize);
+
         finalizeOutput(outList, path, unsupportedUrl);
 
-    /*    }finally {
-            System.out.println(" l 398 driver gets closed");
-            Thread.sleep(2000);
-            driverSubmissions.close();
-            Thread.sleep(2000);
-                try {
-                driverSubmissions.quit();
-            }catch (Exception e){
-                System.out.println("failed on quit 406");
-                e.printStackTrace();
-            }
-
-        }*/
     }
 
     //trim img url remove everything after question mark
